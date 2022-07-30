1587665b2ccbabd2c2c8b29b69a34b3bd6bf87f5
Elianhad <hadad.elian@gmail.com>
2022-07-30 00:01:50 -0300

    cambiando layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import { useState } from 'react'
-import Navegacion from './components/Navegacion'
-import Hero from './components/Hero'
-import Features from './components/Features'
-import Footer from './components/Footer'
-import Contacto from './components/Contacto'
+import { useState } from "react";
+import Navegacion from "./components/Navegacion";
+import Hero from "./components/Hero";
+import Features from "./components/Features";
+import Footer from "./components/Footer";
+import Contacto from "./components/Contacto";
+import Galeria from "./components/Galeria";
 
 function App() {
-
   return (
     <div>
-      <Navegacion/>
-      <Hero/>
-      <Features/>
-      <Contacto/>
-      <Footer/>
-     
+      <Navegacion />
+      <Hero />
+      <Features />
+      <Galeria />
+      <Contacto />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
